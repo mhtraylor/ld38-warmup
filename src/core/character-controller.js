3334b8772a6dc318c4cd9c1a4a2594731219bcfd
Matthew Traylor <mhtraylor@hotmail.com>
2017-04-21 18:27:46 -0500

    fixed eslint

--- src/core/character-controller.js
+++ src/core/character-controller.js
@@ -1,58 +1,49 @@
-import Phaser from 'phaser';
-import Controller from '../core/controller';
-import Animation from '../core/animation';
-
+import Phaser from 'phaser'
+import Controller from '../core/controller'
+import Animation from '../core/animation'
 
 // Enums
-const FacingDirection = { UP: 0, RIGHT: 1, DOWN: 2, LEFT: 3 };
-const CharacterState = { IDLE: 0, WALK: 1 };
+const FacingDirection = { UP: 0, RIGHT: 1, DOWN: 2, LEFT: 3 }
+const CharacterState = { IDLE: 0, WALK: 1 }
 
+export default class CharacterController extends Phaser.Sprite {
+  constructor (game, x, y, asset, controlMap, animations, settings) {
+    super(game, x, y, asset)
 
-export default class CharacterController extends Phaser.Sprite
-{
-    constructor(game, x, y, asset, controlMap, animations, settings)
-    {
-        super(game, x, y, asset);
+    this.directionFacing = FacingDirection.RIGHT
+    this.characterState = CharacterState.IDLE
+    this.isHolding = false
 
-        this.directionFacing = FacingDirection.RIGHT;
-        this.characterState = CharacterState.IDLE;
-        this.isHolding = false;
+    this.settings = settings
+        // this.body.maxVelocity.setTo(this.settings.MAX_SPEED, this.settings.MAX_SPEED); // x, y
+        // this.body.drag.setTo(this.settings.DRAG, this.settings.DRAG); // x, y
 
+    this.anim = new Animation(animations, this)
+    this.ctrl = new Controller(controlMap, this.game)
 
-        this.settings = settings;
-        //this.body.maxVelocity.setTo(this.settings.MAX_SPEED, this.settings.MAX_SPEED); // x, y
-        //this.body.drag.setTo(this.settings.DRAG, this.settings.DRAG); // x, y
+    this.ctrl.moveDown(() => {
+      this.y++
+    })
 
-        this.anim = new Animation(animations, this);
-        this.ctrl = new Controller(controlMap, this.game);
+    this.ctrl.moveUp(() => {
+      this.y--
+    })
 
+    this.ctrl.moveLeft(() => {
+            // this.body.acceleration.x = -this.settings.ACCELERATION;
+      this.x--
+      this.play('walking')
+    })
 
-        this.ctrl.moveDown(() => {
-            this.y++;
-        });
+    this.ctrl.moveRight(() => {
+            // this.body.acceleration.x = this.settings.ACCELERATION;
+      this.x++
+      this.play('walking')
+    })
+  }
 
-        this.ctrl.moveUp(() => {
-            this.y--;
-        });
-
-        this.ctrl.moveLeft(() => {
-            //this.body.acceleration.x = -this.settings.ACCELERATION;
-            this.x--;
-            this.play('walking');
-
-        });
-
-        this.ctrl.moveRight(() => {
-            //this.body.acceleration.x = this.settings.ACCELERATION;
-            this.x++;
-            this.play('walking');
-        });
-    }
-
-
-    update()
-    {
-        this.ctrl.update();
-    }
+  update () {
+    this.ctrl.update()
+  }
 
 }
--- src/sprites/george.js
+++ src/sprites/george.js
@@ -1,47 +1,41 @@
-import Phaser from 'phaser';
-import Controller from '../core/controller';
-import CharacterController from '../core/character-controller';
-import Animation from '../core/animation';
+import Phaser from 'phaser'
+import CharacterController from '../core/character-controller'
 
-export default class George extends CharacterController
-{
-  constructor (game, x, y, asset)
-  {
-        var settings = {
-            MAX_SPEED: 100,
-            ACCELERATION: 20,
-            DRAG: 10
-        };
+export default class George extends CharacterController {
+  constructor (game, x, y, asset) {
+    var settings = {
+      MAX_SPEED: 100,
+      ACCELERATION: 20,
+      DRAG: 10
+    }
 
+    var animations = [
+      {
+        name: 'walking',
+        frames: [0, 1, 2],
+        fps: 8,
+        loop: true
+      }]
 
-        var animations = [
-        {
-            name: 'walking',
-            frames: [0, 1, 2],
-            fps: 8,
-            loop: true
-        }];
+    var controlMap = [
+      {
+        name: 'moveUp',
+        key: Phaser.Keyboard.W
+      },
+      {
+        name: 'moveDown',
+        key: Phaser.Keyboard.S
+      },
+      {
+        name: 'moveLeft',
+        key: Phaser.Keyboard.A
+      },
+      {
+        name: 'moveRight',
+        key: Phaser.Keyboard.D
+      }]
 
-
-        var controlMap = [
-        {
-            name: 'moveUp',
-            key: Phaser.Keyboard.W
-        },
-        {
-            name: 'moveDown',
-            key: Phaser.Keyboard.S
-        },
-        {
-            name: 'moveLeft',
-            key: Phaser.Keyboard.A
-        },
-        {
-            name: 'moveRight',
-            key: Phaser.Keyboard.D
-        }];
-
-        super(game, x, y, asset, controlMap, animations, settings);
-        this.anchor.setTo(0.5);
+    super(game, x, y, asset, controlMap, animations, settings)
+    this.anchor.setTo(0.5)
   }
 }
